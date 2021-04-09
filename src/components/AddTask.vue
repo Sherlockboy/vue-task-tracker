98670acd0bd434919ad8ba7e9f3a4fa24a4bae53
Khurshidbek <kabilovhurshid5@gmail.com>
2021-04-09 10:53:41 +0500

    task-tracker app mvp finished

---
+++ src/components/AddTask.vue
@@ -0,0 +1,85 @@
+<template>
+  <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>Day & Time</label>
+      <input
+        type="text"
+        v-model="day"
+        name="day"
+        placeholder="Add Day & Time"
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>
+        <input type="checkbox" v-model="reminder" name="reminder" />
+        <span>Set Reminder</span>
+      </label>
+    </div>
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddTask",
+  data() {
+    return {
+      text: "",
+      day: "",
+      reminder: false,
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      if (!this.text) {
+        alert("Please add a task");
+        return;
+      }
+
+      const newTask = {
+        id: Math.floor(Math.random() * 100000),
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder,
+      };
+
+      this.$emit("add-task", newTask);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+.form-control {
+  margin: 20px 0;
+}
+.form-control label {
+  display: block;
+}
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+.form-control-check label {
+  display: flex;
+  align-items: center;
+}
+.form-control-check label input {
+  height: 20px;
+  width: 20px;
+  margin-right: 10px;
+}
+</style>
